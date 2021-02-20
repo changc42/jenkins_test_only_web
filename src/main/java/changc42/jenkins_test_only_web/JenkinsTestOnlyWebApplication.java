68e6df66fd8d5ccfbbc68e114f015dd4c4eb9935
Caleb Chang <changc42@gmail.com>
2021-02-20 15:58:11 -0500

    initial commit

---
+++ src/main/java/changc42/jenkins_test_only_web/JenkinsTestOnlyWebApplication.java
@@ -0,0 +1,20 @@
+package changc42.jenkins_test_only_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsTestOnlyWebApplication {
+
+	@GetMapping(path="/")
+	String foo(){
+		return "heyo jeyo, this is the new docker imageg";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestOnlyWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/changc42/jenkins_test_only_web/JenkinsTestOnlyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package changc42.jenkins_test_only_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestOnlyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
